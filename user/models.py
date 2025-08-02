a9217021e3086d4762cf7da585014a086b08822e
HeisenbergOfDjango <workwithaditya01@gmail.com>
2025-08-03 02:54:55 +0530

    update 3

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
